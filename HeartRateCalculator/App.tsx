3f4653b85995261218f4e054e8f3bb10fe89f233
AaPee04 <arttupekka.konttila@gmail.com>
2026-01-09 13:40:51 +0200

    App.tsx and project

--- HeartRateCalculator/App.tsx
+++ HeartRateCalculator/App.tsx
@@ -2,35 +2,41 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-    const [age, setAge] = useState<string>("")
-    const ageasNum: number = !isNaN(Number(age)) === true ? Number(age) : 0
-    const lower: number = (220-ageasNum) * 0.65
-    const upper: number = (220-ageasNum) * 0.85
-    return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Heart Rate limit calculator</Text>
-            <Text style={styles.text}>Enter your age</Text>
-            <TextInput
-                keyboardType="number-pad"
-                value={age}
-                onChangeText={setAge}
-            />
-            <Text style={styles.text}>Lower limit<></>{lower.toFixed(2)} bpm</Text>
-            <Text style={styles.text}>Upper limit<></>{upper.toFixed(2)} bpm</Text>
-        </View>
-    );
+  const [age, setAge] = useState<string>("")
+  const ageasNum: number = !isNaN(Number(age)) === true ? Number(age) : 0
+  const lower: number = (220 - ageasNum) * 0.65
+  const upper: number = (220 - ageasNum) * 0.85
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Heart Rate limit calculator</Text>
+      <Text style={styles.text}>Enter your age</Text>
+      <TextInput
+        keyboardType="number-pad"
+        value={age}
+        onChangeText={setAge}
+      />
+      <Text style={styles.text}>
+        Lower limit {lower.toFixed(2)} bpm
+      </Text>
+
+      <Text style={styles.text}>
+        Upper limit {upper.toFixed(2)} bpm
+      </Text>
+
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 16
-    },
-    heading: {
-        fontSize: 24,
-        marginTop: 32,
-        marginBottom: 16
-    },
-    text: {
-        marginVertical: 16
-    }
+  container: {
+    flex: 1,
+    margin: 16
+  },
+  heading: {
+    fontSize: 24,
+    marginTop: 32,
+    marginBottom: 16
+  },
+  text: {
+    marginVertical: 16
+  }
 });
